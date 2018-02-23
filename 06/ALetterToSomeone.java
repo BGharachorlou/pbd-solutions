06501625c688e2a5591ef35e38a3c99e519c0235
BGharachorlou <36392796+BGharachorlou@users.noreply.github.com>
2018-02-23 10:10:15 -0500

    Add files via upload

---
+++ 06/ALetterToSomeone.java
@@ -0,0 +1,17 @@
+public class ALetterToSomeone
+{
+    public static void main(String[] args)
+        {
+        System.out.println("+---------------------------------------------+");
+        System.out.println("|                                        #### |");
+        System.out.println("|                                        #### |");
+        System.out.println("|                                        #### |");
+        System.out.println("|                                             |");
+        System.out.println("|                                             |");
+        System.out.println("|                  Bardia Gharachorlou        |");
+        System.out.println("|                  Dat Way                    |");
+        System.out.println("|                  Beverley Hills California  |");
+        System.out.println("|                                             |");
+        System.out.println("+---------------------------------------------+");
+    }
+}
