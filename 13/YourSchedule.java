0f5f6d13088a8bc45f3205debfa983316f76815e
BGharachorlou <36392796+BGharachorlou@users.noreply.github.com>
2018-02-28 11:03:07 -0500

    Add files via upload

---
+++ 13/YourSchedule.java
@@ -0,0 +1,12 @@
+public class YourSchedule {
+    public static void main(String[] args) { 
+    
+    System.out.println("+-----------------------------------------------------------------+");
+    System.out.println("| 1 |                              English |        Mr. Sarracini |");
+    System.out.println("| 2 |                                 Math |             Ms. Koon |");
+    System.out.println("| 3 |                                Music |       Mr. DiGirolamo |");
+    System.out.println("| 4 |                              Science |        Mr. Yeghiaian |");
+    System.out.println("+-----------------------------------------------------------------+");
+    
+    }
+}
