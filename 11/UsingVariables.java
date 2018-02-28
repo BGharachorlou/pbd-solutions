d666aa498fa51aa4fd655db6dee665f2341595b5
BGharachorlou <36392796+BGharachorlou@users.noreply.github.com>
2018-02-28 10:02:26 -0500

    Add files via upload

---
+++ 11/UsingVariables.java
@@ -0,0 +1,12 @@
+public class UsingVariables {
+    public static void main(String[] args) {
+        int room = 123;
+        double e = 2.71828; 
+        String myClass = "I am learning a bit about Computer Science"; 
+        
+        System.out.println("This is room " + room);
+        System.out.println("e is close to " + e);
+        System.out.println(myClass);
+    } 
+}
+    
